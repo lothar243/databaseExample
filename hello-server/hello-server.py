99b7d88b39dfd4c5dd339b95da5270a50d2d4cc9
Jeff Arends <jeffrey.arends@mso.umt.edu>
2023-10-20 18:19:47 +0000

    Created example flask server: hello-server.py

---
+++ hello-server/hello-server.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<html><body><h1>Hello, this was rendered directly from Python</h1></body</html>'
+
+@app.route('/premade')
+def premade():
+    output = render_template('hello.html')
+    return output
+
+@app.route('/dynamic')
+def dynamic():
+    output = render_template('dynamic.html', myText="<h1>From Python<h1>")
+    return output
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True, host="0.0.0.0")
