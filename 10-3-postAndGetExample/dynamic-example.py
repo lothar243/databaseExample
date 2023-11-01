0dcdb254aafbc0455d9c7e225489dad9bfab441a
Jeff Arends <jeffrey.arends@mso.umt.edu>
2023-11-01 21:33:46 +0000

    Removed testing 'print' statement

--- 10-3-postAndGetExample/dynamic-example.py
+++ 10-3-postAndGetExample/dynamic-example.py
@@ -29,7 +29,6 @@ def renderFormPost():
 @app.route('/submitformPost.html', methods=['POST'])
 def renderTablePost():
     firstname = request.form.get('fname')
-    print(firstname)
     lastname = request.form.get('lname')
     output = render_template('table.html', firstname=firstname, lastname=lastname)
     return output 
