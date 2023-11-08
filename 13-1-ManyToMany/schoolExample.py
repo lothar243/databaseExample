cb4ef100d4bc6da207af764b0b3c7f7be3682edd
Jeff Arends <jeffrey.arends@mso.umt.edu>
2023-11-08 23:20:58 +0000

    Updating some comments - copy/paste errors

--- 13-1-ManyToMany/schoolExample.py
+++ 13-1-ManyToMany/schoolExample.py
@@ -14,7 +14,7 @@ def showStudents():
     connection = mysql.connector.connect(**creds)
     mycursor = connection.cursor()
 
-    # If there is a name and desc 'GET' variable, insert the new value into the database
+    # If there is a section_id 'GET' variable, use this to refine the query
     sectionID = request.args.get('section_id')
     if sectionID is not None:
         mycursor.execute("""SELECT student.id,first_name,last_name,course_name,course_code from student 
@@ -28,14 +28,12 @@ def showStudents():
             courseNumber = myresult[0][4]
         else:
             courseName = courseNumber = "Unknown"
-        # print(myresult)
         pageTitle = f"Showing all students in section {sectionID}, {courseName} ({courseNumber})"
     else:
         mycursor.execute("SELECT id,first_name,last_name from student")
         pageTitle = "Showing all students"
         myresult = mycursor.fetchall()
 
-    # Fetch the current values of the speaker table
     mycursor.close()
     connection.close()
     return render_template('student.html', studentList=myresult, pageTitle=pageTitle)
@@ -45,7 +43,7 @@ def showSections():
     connection = mysql.connector.connect(**creds)
     mycursor = connection.cursor()
 
-    # If there is a name and desc 'GET' variable, insert the new value into the database
+    # If there is a student_id 'GET' variable, use this to refine the query
     studentID = request.args.get('student_id')
     if studentID is not None:
         mycursor.execute("""SELECT section.id,course_name,course_code,first_name,last_name from student 
@@ -59,7 +57,6 @@ def showSections():
             studentName = myresult[0][2] + " " + myresult[0][3]
         else:
             studentName = "Unknown"
-        # print(myresult)
         pageTitle = f"Showing all sections for student: {studentName})"
     else:
         mycursor.execute("""SELECT section.id, course_name, course_code from section
@@ -67,7 +64,6 @@ def showSections():
         pageTitle = "Showing all sections"
         myresult = mycursor.fetchall()
 
-    # Fetch the current values of the speaker table
     mycursor.close()
     connection.close()
     return render_template('section.html', studentList=myresult, pageTitle=pageTitle)
