f83a9dde39d82e368e6a5f3c5feccc23719b2649
Jeff Arends <Jeff Arends>
2025-10-02 19:30:56 +0000

    added comment about why extensions.py is recommended

--- 12-2-ORMManyToMany/extensions.py
+++ 12-2-ORMManyToMany/extensions.py
@@ -1,2 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+# This is the recommended pattern to create a db object that can be reference both 
+# by the app and by models.py without creating any circular references
