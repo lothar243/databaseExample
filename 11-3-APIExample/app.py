e0df226839f139898e365d33e7886c07fb51d79f
Jeff Arends <Jeff Arends>
2025-10-30 19:27:43 +0000

    Added more to api example

--- 11-3-APIExample/app.py
+++ 11-3-APIExample/app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, request, jsonify
-import mysql.connector, os
 import json
+import mysql.connector
+from flask import Flask, render_template, request, make_response, jsonify
 
 
 app = Flask(__name__)
 
 with open('secrets.json', 'r') as secretFile:
     creds = json.load(secretFile)['mysqlCredentials']
-
 
 @app.route('/api/actors', methods=['GET'])
 def get_actors():
@@ -50,6 +49,60 @@ def get_actor_movies(actor_id: int):
     connection.close()
     return jsonify(actor)
 
+def has_authorization():
+    # Instead of hard-coding this, you should have the username and password store in the database. 
+    # You would also want to check for the appropriate authorization
+    # I'm just putting this here as a proof of concept
+    return request.cookies.get('username') == "jeff" and request.cookies.get('password') == "mypass"
+
+@app.route('/api/actors', methods=['POST'])
+def create_actor():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    if not has_authorization():
+        return jsonify({'error': 'Authentication and authorization required'})
+    if firstname is None or lastname is None:
+        return jsonify({'error': 'Must supply both first and last name for actor'})
+    connection = mysql.connector.connect(**creds)
+    mycursor = connection.cursor()
+    mycursor.execute("Insert into actor (first_name, last_name) values (%s, %s)", (firstname, lastname))
+    connection.commit()
+    row_id = mycursor.lastrowid # get the row id of the newly inserted record
+    mycursor.close()
+    connection.close()
+    return jsonify({'status':'success', 'location': f'/actors/{row_id}'})
+
+@app.route('/api/actors/<int:actor_id>/delete', methods=['DELETE'])
+def delete_actor(actor_id: int):
+    if not has_authorization():
+        return jsonify({'error': 'Authentication and authorization required'})
+    connection = mysql.connector.connect(**creds)
+    mycursor = connection.cursor()
+    mycursor.execute("delete from actor where actor_id=%s", (actor_id,))
+    connection.commit()
+    mycursor.close()
+    connection.close()
+    return jsonify({'status': 'success'})
+
+    
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login():
+    response = make_response('You are now logged in<br><a href="/">Return</a>')
+    response.set_cookie('username', 'jeff')
+    response.set_cookie('password', 'mypass')
+    return response
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    response = make_response('You are now logged out<br><a href="/">Return</a>')
+    response.set_cookie('username', '', expires=0)
+    response.set_cookie('password', '', expires=0)
+    return response
 
 
 if __name__ == '__main__':
